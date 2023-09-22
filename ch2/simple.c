5219024e4d0248d95e734f111c9d8a00d5d34659
Fox Johnjulio <foxkj01@gmail.com>
2023-09-22 18:55:18 -0400

    part1

--- ch2/simple.c
+++ ch2/simple.c
@@ -12,18 +12,28 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/hash.h>
+#include <linux/jiffies.h>
+#include <linux/gcd.h>
+#include <linux/param.h>
 
 /* This function is called when the module is loaded. */
 int simple_init(void)
 {
        printk(KERN_INFO "Loading Module\n");
+       printk(KERN_INFO "Golden Ratio Prime: %llu\n", GOLDEN_RATIO_PRIME)
+       printk(KERN_INFO "HZ: %lu\n", HZ)
+       printk(KERN_INFO "Jiffies: %lu\n", jiffies)
 
-       return 0;
+    return 0;
 }
 
 /* This function is called when the module is removed. */
 void simple_exit(void) {
 	printk(KERN_INFO "Removing Module\n");
+    printk(KERN_INFO "GCD(3300, 24) = %lu\n", gcd(3300,24))
+    printk(KERN_INFO "Jiffies: %lu\n", jiffies)
+
 }
 
 /* Macros for registering module entry and exit points. */
